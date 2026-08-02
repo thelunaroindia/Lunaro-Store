48e1329aecaf28cb8af3da0dca57cf9fbc82bbe7
Mayank Yadav <mayankyadavbtech24@rvu.edu.in>
2026-08-02 13:43:37 +0530

    Refine collection rail and update Graphic Tees artwork

--- src/components/home/CollectionCarousel.tsx
+++ src/components/home/CollectionCarousel.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { Reveal } from '@/components/motion/Reveal';
 
 type CollectionCard = {
   name: string;
@@ -22,30 +21,29 @@ export default function CollectionCarousel() {
       aria-label="Shop by collection"
       className="border-t border-graphite py-16 md:py-28"
     >
-      <div className="container-lunaro">
-        <Reveal>
-          <div className="grid grid-flow-col auto-cols-[80vw] gap-3 overflow-x-auto snap-x snap-mandatory scroll-smooth pb-1 lg:grid-flow-row lg:auto-cols-auto lg:grid-cols-5 lg:gap-3 lg:overflow-visible lg:snap-none lg:pb-0">
-            {COLLECTIONS.map((collection) => (
-              <Link
-                key={collection.href}
-                href={collection.href}
-                className="group relative block aspect-[4/5] snap-center overflow-hidden bg-charcoal"
-              >
-                <Image
-                  src={collection.image}
-                  alt={`${collection.name} collection`}
-                  fill
-                  sizes="(min-width: 1024px) 20vw, 80vw"
-                  className="object-cover transition-transform duration-[1100ms] ease-lunar group-hover:scale-[1.045]"
-                />
+      <div
+        className="flex gap-3 overflow-x-auto snap-x snap-mandatory scroll-smooth pl-5 pr-0 scroll-pl-5 sm:pl-8 sm:scroll-pl-8 lg:pl-12 lg:scroll-pl-12 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden"
+      >
+        {COLLECTIONS.map((collection, index) => (
+          <Link
+            key={collection.href}
+            href={collection.href}
+            className="group relative block aspect-[4/5] w-[82vw] shrink-0 snap-start overflow-hidden bg-charcoal sm:w-[46vw] lg:w-[28vw] lg:max-w-[420px]"
+          >
+            <Image
+              src={collection.image}
+              alt={`${collection.name} collection`}
+              fill
+              priority={index < 2}
+              sizes="(min-width: 1024px) 30vw, (min-width: 640px) 46vw, 82vw"
+              className="object-cover transition-transform duration-[1100ms] ease-lunar group-hover:scale-[1.045]"
+            />
 
-                <span className="absolute bottom-4 left-4 bg-obsidian/60 px-3 py-1.5 font-display text-lg uppercase tracking-wider2 text-lunar sm:text-xl">
-                  {collection.name}
-                </span>
-              </Link>
-            ))}
-          </div>
-        </Reveal>
+            <span className="absolute bottom-4 left-4 font-display text-lg uppercase tracking-wider2 text-lunar [filter:drop-shadow(0_2px_8px_rgba(0,0,0,0.85))] sm:bottom-5 sm:left-5 sm:text-xl lg:bottom-6 lg:left-6">
+              {collection.name}
+            </span>
+          </Link>
+        ))}
       </div>
     </section>
   );
